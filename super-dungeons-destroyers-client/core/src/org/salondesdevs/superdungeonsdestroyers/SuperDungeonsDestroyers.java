ca95c857e1ab1e8442c125de716f2b2526a0cc67
BuildTools <unconfigured@null.spigotmc.org>
2019-07-13 16:49:06 +0200

    Client: added ecs lib and begun base structure

--- super-dungeons-destroyers-client/core/src/org/salondesdevs/superdungeonsdestroyers/SuperDungeonsDestroyers.java
+++ super-dungeons-destroyers-client/core/src/org/salondesdevs/superdungeonsdestroyers/SuperDungeonsDestroyers.java
@@ -5,29 +5,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import net.wytrem.ecs.*;
 
 public class SuperDungeonsDestroyers extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	World world;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		WorldConfiguration worldConfiguration = new WorldConfiguration();
+		worldConfiguration.register(RenderingSystem.class);
+
+		world = new World(worldConfiguration);
+		world.initialize();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		world.process(Gdx.graphics.getDeltaTime());
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		world.dispose();
 	}
 }
