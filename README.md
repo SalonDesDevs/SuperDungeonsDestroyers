bb4c9e127450883c7f0ba207008d6222afa65db7
Adrien Navratil <adrien1975@live.fr>
2019-07-13 16:07:43 +0200

    Base LibGDX and Rust project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Super Dungeons Destroyers
+
---
+++ super-dungeons-destroyers-client/desktop/src/org/salondesdevs/superdungeonsdestroyers/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.salondesdevs.superdungeonsdestroyers.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.salondesdevs.superdungeonsdestroyers.SuperDungeonsDestroyers;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperDungeonsDestroyers(), config);
+	}
+}
