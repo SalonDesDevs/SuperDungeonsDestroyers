49d7bd9d38b0c2c02a77e4e06af911a2c31c2ed1
LightDiscord <root@arnaud.sh>
2019-07-13 21:31:31 +0200

    feat: tokio 0.1

--- super-dungeons-destroyers-server/src/main.rs
+++ super-dungeons-destroyers-server/src/main.rs
@@ -1,29 +1,26 @@
-#![feature(async_await)]
-#![deny(warnings, rust_2018_idioms)]
-use sdd::player::{ get_root_as_player, Player, PlayerArgs, Vec3 };
-use flatbuffers::FlatBufferBuilder;
-use tokio;
-use tokio::io::AsyncWriteExt;
+#![feature(async_await, vec_remove_item)]
+
+use tokio::prelude::*;
 use tokio::net::TcpListener;
-#[tokio::main]
-async fn main() {
-    let mut builder = FlatBufferBuilder::new();
-    let addr = "127.0.0.1:9000".parse().unwrap();
-    let listener = TcpListener::bind(&addr).unwrap();
-    let name = builder.create_string("John Doe");
+use failure::Fallible;
 
-    let player = Player::create(
-        &mut builder,
-        &PlayerArgs {
-            name: Some(name),
-            location: Some(&Vec3::new(0f32, 0f32, 0f32))
-        }
-    );
+fn listener() -> Fallible<TcpListener> {
+    let address = "127.0.0.1:9000".parse()?;
 
-    builder.finish(player, None);
-    
-    let buffer = builder.finished_data();
-    let player = get_root_as_player(buffer);
+    Ok(TcpListener::bind(&address)?)
+}
 
-    println!("Player's name is {}", player.name());
+fn main() -> Fallible<()> {
+    let server = listener()?
+        .incoming()
+        .map_err(|_error| ())
+        .for_each(move |_socket| {
+            println!("New socket");
+
+            Ok(())
+        });
+
+    tokio::run(server);
+
+    Ok(())
 }
