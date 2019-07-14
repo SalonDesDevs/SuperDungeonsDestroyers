2f296d72452848fedddea5e93cee4c6f365113ad
wytrem <wytrem@lilo.org>
2019-07-14 17:32:40 +0200

    Client: networking basics

--- super-dungeons-destroyers-client/core/src/org/salondesdevs/superdungeonsdestroyers/systems/connectingtoserver/NetworkSystem.java
+++ super-dungeons-destroyers-client/core/src/org/salondesdevs/superdungeonsdestroyers/systems/connectingtoserver/NetworkSystem.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.net.Socket;
 import com.badlogic.gdx.net.SocketHints;
 import net.wytrem.ecs.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class NetworkSystem extends BaseSystem {
 
     private Socket clientSocket;
@@ -13,6 +16,44 @@ public class NetworkSystem extends BaseSystem {
     @Override
     public void initialize() {
         this.clientSocket = Gdx.net.newClientSocket(Net.Protocol.TCP, "localhost", 9000, new SocketHints());
+
+        new Thread("Socket listener") {
+            @Override
+            public void run() {
+
+                InputStream inputStream = clientSocket.getInputStream();
+
+                long size;
+                byte[] sizeBuffer = new byte[4];
+                byte[] buffer = new byte[1024];
+
+                try {
+
+                    while (true) {
+                        if (inputStream.read(sizeBuffer) != 1) {
+                            size = 0;
+                            for (int i = 0; i < 4; i++) {
+                                size <<= 8;
+                                size |= (sizeBuffer[i] & 0xFF);
+                            }
+
+//                            System.out.println("size = ");
+
+                            if (buffer.length < size) {
+                                buffer = new byte[(int) size];
+                            }
+
+                            inputStream.read(buffer, 0, (int) size);
+
+                        }
+
+                    }
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+
+            }
+        }.start();
     }
 
     @Override
