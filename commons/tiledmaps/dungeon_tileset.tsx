523ed0a01cde214e6d3434d6dda60146b498deeb
wytrem <wytrem@lilo.org>
2019-07-15 21:47:00 +0200

    Updated example Tiledmap

--- commons/tiledmaps/dungeon_tileset.tsx
+++ commons/tiledmaps/dungeon_tileset.tsx
@@ -86,6 +86,30 @@
    <property name="solid" value="true"/>
   </properties>
  </tile>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="130"/>
+   <frame tileid="153" duration="130"/>
+   <frame tileid="154" duration="130"/>
+   <frame tileid="155" duration="130"/>
+   <frame tileid="156" duration="130"/>
+   <frame tileid="157" duration="130"/>
+   <frame tileid="158" duration="130"/>
+   <frame tileid="159" duration="130"/>
+  </animation>
+ </tile>
+ <tile id="168">
+  <animation>
+   <frame tileid="168" duration="130"/>
+   <frame tileid="169" duration="130"/>
+   <frame tileid="170" duration="130"/>
+   <frame tileid="171" duration="130"/>
+   <frame tileid="172" duration="130"/>
+   <frame tileid="173" duration="130"/>
+   <frame tileid="174" duration="130"/>
+   <frame tileid="175" duration="130"/>
+  </animation>
+ </tile>
  <tile id="205">
   <properties>
    <property name="solid" value="true"/>
