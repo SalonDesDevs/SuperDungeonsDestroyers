a2e7798906a7593e1c5f6db9c138a932d99a7066
Navatar <teo.nespoulet@gmail.com>
2019-07-15 15:56:52 +0200

    adding new schema

--- super-dungeons-destroyers-server/build.rs
+++ super-dungeons-destroyers-server/build.rs
@@ -10,8 +10,11 @@ fn main() -> Result<()> {
 
     run(Args {
         inputs: &[
+            Path::new("../schemas/dungeon.fbs"),
             Path::new("../schemas/player.fbs"),
-
+            Path::new("../schemas/pickable.fbs"),
+            Path::new("../schemas/mob.fbs"),
+            
             Path::new("../schemas/client.fbs"),
             Path::new("../schemas/server.fbs"),
         ],
--- super-dungeons-destroyers-server/src/binding.rs
+++ super-dungeons-destroyers-server/src/binding.rs
@@ -5,6 +5,24 @@ mod player_generated;
 pub use player_generated::sdd::player;
 
 #[allow(non_snake_case, unused_imports)]
+#[path = "../../target/flatbuffers/mob_generated.rs"]
+mod mob_generated;
+
+pub use mob_generated::sdd::mob;
+
+#[allow(non_snake_case, unused_imports)]
+#[path = "../../target/flatbuffers/dungeon_generated.rs"]
+mod dungeon_generated;
+
+pub use dungeon_generated::sdd::mob;
+
+#[allow(non_snake_case, unused_imports)]
+#[path = "../../target/flatbuffers/pickable_generated.rs"]
+mod pickale_generated;
+
+pub use pickable_generated::sdd::mob;
+
+#[allow(non_snake_case, unused_imports)]
 #[path = "../../target/flatbuffers/client_generated.rs"]
 mod client_generated;
 
