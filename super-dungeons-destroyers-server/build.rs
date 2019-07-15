53deed0a9e6e69bd877002f5fe3fd920d24968bf
wytrem <wytrem@lilo.org>
2019-07-15 16:35:31 +0200

    creaded the commons folder

--- super-dungeons-destroyers-server/build.rs
+++ super-dungeons-destroyers-server/build.rs
@@ -3,17 +3,17 @@ use std::io::Result;
 use flatc_rust::{ run, Args };
 
 fn main() -> Result<()> {
-    println!("cargo:rerun-if-changed=../schemas/player.fbs");
+    println!("cargo:rerun-if-changed=../commons/schemas/player.fbs");
 
-    println!("cargo:rerun-if-changed=../schemas/client.fbs");
-    println!("cargo:rerun-if-changed=../schemas/server.fbs");
+    println!("cargo:rerun-if-changed=../commons/schemas/client.fbs");
+    println!("cargo:rerun-if-changed=../commons/schemas/server.fbs");
 
     run(Args {
         inputs: &[
-            Path::new("../schemas/player.fbs"),
+            Path::new("../commons/schemas/player.fbs"),
 
-            Path::new("../schemas/client.fbs"),
-            Path::new("../schemas/server.fbs"),
+            Path::new("../commons/schemas/client.fbs"),
+            Path::new("../commons/schemas/server.fbs"),
         ],
         out_dir: &Path::new("../target/flatbuffers"),
         ..Default::default()
