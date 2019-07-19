0b43cd53776290b00f6f8372f56e384f1b01c02f
wytrem <wytrem@lilo.org>
2019-07-19 20:16:55 +0200

    Client: bumped gdx backed to lwjgl3

--- super-dungeons-destroyers-client/desktop/src/org/salondesdevs/superdungeonsdestroyers/desktop/DesktopLauncher.java
+++ super-dungeons-destroyers-client/desktop/src/org/salondesdevs/superdungeonsdestroyers/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package org.salondesdevs.superdungeonsdestroyers.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import org.salondesdevs.superdungeonsdestroyers.SuperDungeonsDestroyersClient;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new SuperDungeonsDestroyersClient(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        new Lwjgl3Application(new SuperDungeonsDestroyersClient(), config);
     }
 }
